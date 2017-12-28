b63da986a3eaca025f1508b40d477eeabbdbc1f2
Patrick <pat.rodriguez108@gmail.com>
2017-12-27 21:03:43 -0600

    Start fresh

--- palindrome_too.rb
+++ palindrome_too.rb
@@ -5,22 +5,7 @@
 # return index of last element of array
 
 def min_cut(s)
-	letters = []
-	i = 0
-	while i < s.length
-		letters << s[i]
-		if s[i + 1].class == String
-			if s[i] + s[i + 1] == (s[i] + s[i + 1]).reverse
-				letters << s[i] + s[i + 1]
-			else
-				letters << s[i]
-			end
-		else
-			letters << s[i]
-		end
-		i += 1
-	end
-	p letters
+
 end
 
 # example
